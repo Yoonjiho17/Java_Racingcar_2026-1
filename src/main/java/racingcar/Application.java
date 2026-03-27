c8dabab7fb52dcbcdd3a6b8945681fa8bbf7fdfb
Yoon JiHo <dbswlgh0627@gamil.com>
2026-03-28 00:06:12 +0900

    feat: 예외처리 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -12,9 +12,18 @@ public class Application {
 
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String carName = input.nextLine();
-        String tokens[] = carName.split(",");
-        for (int i = 0; i < tokens.length; i++) {
-            arrayList.add(0);
+        String[] tokens = carName.split(",");
+
+        try {
+            for (int i = 0; i < tokens.length; i++) {
+                if (tokens[i].length() > 5) {
+                    throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
+                }
+                arrayList.add(0);
+            }
+        } catch (IllegalArgumentException e) {
+            System.err.println("오류 발생: " + e.getMessage());
+            System.exit(0);
         }
 
         System.out.println("시도할 회수는 몇회인가요?");
