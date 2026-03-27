2ef3bd595358a96858ccc7b50b165267f3533b4c
Yoon JiHo <dbswlgh0627@gamil.com>
2026-03-27 22:52:18 +0900

    feat: 자동차 전진 기능 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,29 +1,48 @@
 package racingcar;
 
+import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        ArrayList<Integer> arrayList = new ArrayList<>(); // 전진상황 리스트
 
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String carName = input.nextLine();
         String tokens[] = carName.split(",");
+        for (int i = 0; i < tokens.length; i++) {
+            arrayList.add(0);
+        }
 
         System.out.println("시도할 회수는 몇회인가요?");
         int racingCounts = input.nextInt();
 
         System.out.println("\n실행 결과");
-        startRacing(tokens, racingCounts);
+        arrayList = startRacing(tokens, racingCounts, arrayList);
     }
 
-    public static void startRacing(String[] tokens, int counts) {
-        for (int i = 5; i <= counts; i++) {
-            for (String x : tokens) {
-                System.out.print(x + " : ");
-                //랜덤한 출력을 해줄 함수
-                System.out.println();
+    public static ArrayList<Integer> startRacing(String[] tokens, int counts, ArrayList<Integer> arrayList) {
+        for (int i = 1; i <= counts; i++) {
+            for (int j=0; j < tokens.length; j++) {
+                System.out.print(tokens[j] + " : ");
+                arrayList = goOrStop(j, arrayList);
+                System.out.println("- ".repeat(arrayList.get(j)));
             }
+            System.out.println();
         }
+
+        return arrayList;
+    }
+
+    public static ArrayList<Integer> goOrStop(int index, ArrayList<Integer> arrayList) {
+        Random random = new Random();
+        int randomNum = random.nextInt(10);
+        if (randomNum >= 4) {
+            arrayList.set(index, arrayList.get(index) + 1);
+        }
+
+        return arrayList;
     }
 }
