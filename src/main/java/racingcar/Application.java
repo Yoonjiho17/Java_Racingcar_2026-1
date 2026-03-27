f8158ca32590264b351388d5c2e2fb56463d5d14
Yoon JiHo <dbswlgh0627@gamil.com>
2026-03-27 22:27:06 +0900

    feat: 차수별 실행 결과 출력 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -8,7 +8,22 @@ public class Application {
 
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String carName = input.nextLine();
+        String tokens[] = carName.split(",");
+
         System.out.println("시도할 회수는 몇회인가요?");
         int racingCounts = input.nextInt();
+
+        System.out.println("\n실행 결과");
+        startRacing(tokens, racingCounts);
+    }
+
+    public static void startRacing(String[] tokens, int counts) {
+        for (int i = 5; i <= counts; i++) {
+            for (String x : tokens) {
+                System.out.print(x + " : ");
+                //랜덤한 출력을 해줄 함수
+                System.out.println();
+            }
+        }
     }
 }
