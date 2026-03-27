5782e255fbe51a95f071a746f624f5ea5d7c2ebc
Yoon JiHo <dbswlgh0627@gamil.com>
2026-03-27 21:15:45 +0900

    feat: 입력 기능 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,14 @@
 package racingcar;
 
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String carName = input.nextLine();
+        System.out.println("시도할 회수는 몇회인가요?");
+        int racingCounts = input.nextInt();
     }
 }
