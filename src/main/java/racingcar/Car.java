ed3c3a326c40df9eb0835e371912238dc5632bf7
Yoon JiHo <dbswlgh0627@gamil.com>
2026-04-04 15:18:02 +0900

    refactor: Car 클래스 작성

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,15 @@
+package racingcar;
+
+public class Car {
+    private String carName;
+    private int movement;
+
+    public Car(String carName) {
+        this.carName = carName;
+        this.movement = 0;
+    }
+
+    public void move() {
+        movement += 1;
+    }
+}
