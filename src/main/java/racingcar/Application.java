2cbf37c54837d756dc33efe1edd377ffda162dcd
Yoon JiHo <dbswlgh0627@gamil.com>
2026-04-04 17:25:06 +0900

    refactor: Application 클래스 리팩토링, View 클래스 우승자 출력 작성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,37 +1,46 @@
 package racingcar;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashSet;
 
-import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
     public static void main(String[] args) {
-        ArrayList<Integer> arrayList = new ArrayList<>(); // 전진상황 리스트
         HashSet<String> uniqueElements = new HashSet<>();
 
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String carName = readLine();
 
-        nameException(carName);
+        nameException(carName); // 예외처리
 
         String[] tokens = carName.split(",");
 
-        equalException(tokens, uniqueElements);
-        arrayList = exception(tokens, arrayList);
+        equalException(tokens, uniqueElements); // 예외처리
+        exception(tokens); // 예외처리
+
+        Car[] cars = new Car[tokens.length];
+        for (int i = 0; i < tokens.length; i++) {
+            cars[i] = new Car(tokens[i]);
+        }
 
         System.out.println("시도할 횟수는 몇 회인가요?");
         String racingCount = readLine();
-        int counts = tryCountNumCheckException(racingCount);
-        tryCountException(counts);
+        int counts = tryCountNumCheckException(racingCount); // 예외처리
+        tryCountException(counts); // 예외처리
 
         System.out.println();
         System.out.println("실행 결과");
-        arrayList = startRacing(tokens, counts, arrayList);
+        Racing race = new Racing();
+        View view = new View();
+        for (int i = 0; i < counts; i++) {
+            for (int j = 0; j < tokens.length; j++) {
+                race.goOrStrop(cars[j]);
+                view.racingCurrentView(cars[j]);
+            }
+            System.out.println();
+        }
 
-        winner(tokens, arrayList);
+        view.winner(cars);
     }
 
     public static void nameException(String carName) {
@@ -44,7 +53,7 @@ public class Application {
         }
     }
 
-    public static ArrayList<Integer> exception(String[] tokens, ArrayList<Integer> arrayList) {
+    public static void exception(String[] tokens) {
         if (tokens.length == 0) {
             throw new IllegalArgumentException("자동차를 적어야합니다.");
         }
@@ -58,10 +67,7 @@ public class Application {
             if (tokens[i].contains(" ")) {
                 throw new IllegalArgumentException("자동차 이름에 공백은 불가능합니다.");
             }
-            arrayList.add(0);
         }
-
-        return arrayList;
     }
 
     public static void equalException(String[] tokens, HashSet<String> uniqueElements) {
@@ -85,43 +91,5 @@ public class Application {
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("시도 횟수는 숫자여야합니다.");
         }
-    }
-
-    public static ArrayList<Integer> startRacing(String[] tokens, int counts, ArrayList<Integer> arrayList) {
-        for (int i = 1; i <= counts; i++) {
-            for (int j=0; j < tokens.length; j++) {
-                System.out.print(tokens[j] + " : ");
-                arrayList = goOrStop(j, arrayList);
-                System.out.println("-".repeat(arrayList.get(j)));
-            }
-            System.out.println();
-        }
-
-        return arrayList;
-    }
-
-    public static ArrayList<Integer> goOrStop(int index, ArrayList<Integer> arrayList) {
-        int randomNum = pickNumberInRange(0,9);
-        if (randomNum >= 4) {
-            arrayList.set(index, arrayList.get(index) + 1);
-        }
-
-        return arrayList;
-    }
-
-    public static void winner(String[] tokens, ArrayList<Integer> arrayList) {
-        int max = Collections.max(arrayList);
-        StringBuilder sb = new StringBuilder();
-
-        for (int i = 0; i < arrayList.size(); i++) {
-            if (arrayList.get(i) == max) {
-                if (sb.length() > 0) {
-                    sb.append(", ");
-                }
-                sb.append(tokens[i]);
-            }
-        }
-
-        System.out.println("최종 우승자 : " + sb);
     }
 }
--- src/main/java/racingcar/View.java
+++ src/main/java/racingcar/View.java
@@ -5,4 +5,26 @@ public class View {
     public void racingCurrentView(Car car) {
         System.out.println(car.checkCarName() + " : " + "-".repeat(car.checkMovement()));
     }
+
+    public void winner(Car[] cars) {
+        int max = cars[0].checkMovement();
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < cars.length; i++) {
+            if (max < cars[i].checkMovement()) {
+                max = cars[i].checkMovement();
+            }
+        }
+
+        for (int j = 0; j < cars.length; j++) {
+            if (cars[j].checkMovement() == max) {
+                if (sb.length() > 0) {
+                    sb.append(", ");
+                }
+                sb.append(cars[j].checkCarName());
+            }
+        }
+
+        System.out.println("최종 우승자 : " + sb);
+    }
 }
