725854b53d2b302c2a94d11629a447c9141c6b29
Yoon JiHo <dbswlgh0627@gamil.com>
2026-04-06 19:43:51 +0900

    refactor: RacingController 클래스 작성, 패키지 생성 및 분류

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,31 +1,9 @@
 package racingcar;
 
-import java.util.HashSet;
-
-import static camp.nextstep.edu.missionutils.Console.readLine;
+import racingcar.controller.RacingController;
 
 public class Application {
     public static void main(String[] args) {
-        HashSet<String> uniqueElements = new HashSet<>();
-
-        String carName = InputView.getCarName();
-
-        InputValidator.validCarName(carName); // 예외처리
-
-        String[] tokens = carName.split(",");
-
-        Cars.validEqualName(tokens, uniqueElements); // 예외처리
-
-        Car[] cars = Cars.rappingCars(tokens);
-
-        String racingCount = InputView.getTryCount();
-        int counts = InputValidator.validTryCountNum(racingCount); // 예외처리
-        InputValidator.validTryCount(counts); // 예외처리
-
-        Racing race = new Racing();
-        OutputView.viewStartResult(tokens, counts, cars, race);
-
-        OutputView view = new OutputView();
-        view.viewWinner(cars);
+        RacingController.startRacing();
     }
 }
---
+++ src/main/java/racingcar/controller/RacingController.java
@@ -0,0 +1,37 @@
+package racingcar.controller;
+
+import racingcar.util.InputValidator;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+import racingcar.domain.Racing;
+import racingcar.domain.Car;
+import racingcar.domain.Cars;
+
+import java.util.HashSet;
+
+public class RacingController {
+
+    public static void startRacing() {
+        HashSet<String> uniqueElements = new HashSet<>();
+
+        String carName = InputView.getCarName();
+
+        InputValidator.validCarName(carName); // 예외처리
+
+        String[] tokens = carName.split(",");
+
+        Cars.validEqualName(tokens, uniqueElements); // 예외처리
+
+        Car[] cars = Cars.rappingCars(tokens);
+
+        String racingCount = InputView.getTryCount();
+        int counts = InputValidator.validTryCountNum(racingCount); // 예외처리
+        InputValidator.validTryCount(counts); // 예외처리
+
+        Racing race = new Racing();
+        OutputView.viewStartResult(tokens, counts, cars, race);
+
+        OutputView view = new OutputView();
+        view.viewWinner(cars);
+    }
+}
