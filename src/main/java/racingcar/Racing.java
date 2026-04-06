847980fda90e3211b53e48a6159d247d956dbd39
Yoon JiHo <dbswlgh0627@gamil.com>
2026-04-06 15:53:29 +0900

    refactor: 자동차 움직이는 조건 상수로 변경

--- src/main/java/racingcar/Racing.java
+++ src/main/java/racingcar/Racing.java
@@ -3,10 +3,11 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Racing {
+    private static final int CAR_MOVE_STANDARD = 4;
 
     public void goOrStop(Car car) {
         int randomNum = Randoms.pickNumberInRange(0, 9);
-        if (randomNum >= 4) {
+        if (randomNum >= CAR_MOVE_STANDARD) {
             car.move();
         }
     }
